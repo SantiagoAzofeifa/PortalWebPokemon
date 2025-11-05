2bb86fd62a8677a0550dd02c63bf354ca8a11dca
SANTIAGO AZOFEIFA BENAVIDES <santiago.azofeifa.benavides@est.una.ac.cr>
2025-11-04 22:09:29 -0600

    inicial

---
+++ src/main/java/cr/ac/una/portalwebpokeapi/PortalWebPokeApiApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.portalwebpokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalWebPokeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalWebPokeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cr/ac/una/portalwebpokeapi/PortalWebPokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.portalwebpokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalWebPokeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
